5d7b2c93b56e9d514d9c0aae530dc62a2e81a3da
Ray Li <ray.li.verified@gmail.com>
2018-09-03 10:36:09 -0500

    Update Android X
    *Update to Android X.

--- app/src/main/java/xyz/hanks/numberanimation/MainActivity.java
+++ app/src/main/java/xyz/hanks/numberanimation/MainActivity.java
@@ -1,7 +1,7 @@
 package xyz.hanks.numberanimation;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 import xyz.hanks.library.NumberView;
